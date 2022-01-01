9bfde4d420c772d84006417e58440fec8b2ad5ac
andre.h.dos.santos <andre.h.dos.santos@avanade.com>
2022-01-01 16:07:18 -0300

    Add db, models, routes

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const express = require('express');
-const app = express();
+const express       = require('express');
+const app           = express();
+const db            = require('./db/connection');
+const bodyParser    = require('body-parser');
+
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`Server running. PORT: ${PORT}`);
 });
 
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+//db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("DB connection OK!");
+    })
+    .catch(err => {
+        console.log("DB connection failed! ERROR: ", err);
+    });
+
+//routes
 app.get('/', (req, res) => {
     res.send("OK!");
 });
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
